19dacd72d7b5982298c801cf4bc2741c3cb0ab0f
yusufariturkk <yusufariturkk@aof.anadolu.edu.tr>
2020-03-24 18:00:34 +0300

    Initial Commit

---
+++ instagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by Yusuf ARİTÜRK on 24.03.2020.
+//  Copyright © 2020 Yusuf ARİTÜRK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
